f6d2cb6b9e16c39f8f60489eccd4f9b6c31e9d88
yw9142 <yw9142@gmail.com>
2022-02-14 22:02:47 +0900

    feat: SegmentedControl.js impl MUI Tab 기반 Segmented UI 작업 중

--- src/components/common/SegmentedControl.js
+++ src/components/common/SegmentedControl.js
@@ -1,23 +1,89 @@
-import React, {useState} from "react";
-import {styled} from "@mui/material/styles";
-import Tabs from "@mui/material/Tabs";
-import Tab from '@mui/material/Tab';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import TabsUnstyled from '@mui/base/TabsUnstyled';
+import TabsListUnstyled from '@mui/base/TabsListUnstyled';
+import TabPanelUnstyled from '@mui/base/TabPanelUnstyled';
+import { buttonUnstyledClasses } from '@mui/base/ButtonUnstyled';
+import TabUnstyled, { tabUnstyledClasses } from '@mui/base/TabUnstyled';
 
+const blue = {
+    50: '#F0F7FF',
+    100: '#C2E0FF',
+    200: '#80BFFF',
+    300: '#66B2FF',
+    400: '#3399FF',
+    500: '#007FFF',
+    600: '#0072E5',
+    700: '#0059B2',
+    800: '#004C99',
+    900: '#003A75',
+};
 
-function SegmentedControl(props) {
-    const [value, setValue] = useState(0);
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-    console.log(value);
+const Tab = styled(TabUnstyled)`
+  color: white;
+  cursor: pointer;
+  font-size: 0.875rem;
+  font-weight: bold;
+  background-color: transparent;
+  width: 100%;
+  padding: 12px 16px;
+  margin: 6px 6px;
+  border: none;
+  border-radius: 100px;
+  display: flex;
+  justify-content: center;
 
+  &:hover {
+    background-color: ${blue[400]};
+  }
+
+  &:focus {
+    color: #fff;
+    outline: 2px solid ${blue[200]};
+    outline-offset: 2px;
+    border-radius: 100px;
+  }
+
+  &.${tabUnstyledClasses.selected} {
+    background-color: ${blue[50]};
+    color: ${blue[600]};
+    border-radius: 100px;
+  }
+
+  &.${buttonUnstyledClasses.disabled} {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
+
+const TabPanel = styled(TabPanelUnstyled)`
+  width: 100%;
+  font-size: 0.875rem;
+  border-radius: 100px;
+`;
+
+const TabsList = styled(TabsListUnstyled)`
+  min-width: 320px;
+  background-color: ${blue[500]};
+  border-radius: 100px;
+  margin-bottom: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-content: space-between;
+`;
+
+export default function SegmentedControl() {
     return (
-        <Tabs value={value} onChange={handleChange} centered>
-            <Tab label="Item One"/>
-            <Tab label="Item Two"/>
-            <Tab label="Item Three"/>
-        </Tabs>
+        <TabsUnstyled defaultValue={0}>
+            <TabsList>
+                <Tab>One</Tab>
+                <Tab>Two</Tab>
+                <Tab>Three</Tab>
+            </TabsList>
+            <TabPanel value={0}>First content</TabPanel>
+            <TabPanel value={1}>Second content</TabPanel>
+            <TabPanel value={2}>Third content</TabPanel>
+        </TabsUnstyled>
     );
 }
-
-export default SegmentedControl;
