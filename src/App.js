618afcb49d8d01fac70ba6dacc59663d698c52c2
song127 <thdtmdwo789@naver.com>
2022-02-19 14:20:37 +0900

    Fin - Finish Earn Page

--- src/App.js
+++ src/App.js
@@ -35,8 +35,9 @@ function App() {
 
                     <Routes>
                         <Route path="/" element={<Page.Components/>}/>
-                        <Route path="/basset" element={<Page.BAsset/>}/>
+                        <Route path="/earn" element={<Page.Earn/>}/>
                         <Route path="/borrow" element={<Page.Borrow/>}/>
+                        <Route path="/basset" element={<Page.BAsset/>}/>
                         <Route path="/gov" element={<Page.Govern/>}/>
                     </Routes>
 
