e1f89a49176ecd4fca3b7aac5bd678ae59578d7f
Yonghun Park <yw9142@gmail.com>
2022-02-19 15:43:34 +0900

    impl: BorrowTemplate
    ui 완료 / modal 작업만 남음

--- src/components/borrow/BorrowTemplate.js
+++ src/components/borrow/BorrowTemplate.js
@@ -1,21 +1,30 @@
 import React, {useState} from "react";
 import styled from "styled-components";
 import {ReactComponent as LinkMark} from "assets/Link.svg";
+import {ReactComponent as BNear} from "assets/bNear.svg";
 import {ReactComponent as Liquidation} from "assets/Liquidation.svg";
 import {Link} from 'react-router-dom';
 import CommonButton from "components/common/CommonButton";
-import ButtonIcon from "../common/ButtonIcon";
+import ButtonIcon from "components/common/ButtonIcon";
 import BorrowBoard from "./borrowBoard";
+import SecondBorrowBoard from "./SecondBorrowBoard";
+import TransitionAlerts from "components/alert/Alert";
 
 const StyledLink = styled(LinkMark)`
   margin: 0 0 7px 5px;
   vertical-align: bottom;
 `;
 
-const ButtonContainer = styled('div')`
+const ButtonContainer = styled.div`
   display: flex;
   justify-content: flex-end;
 `;
+
+const CardButtonContainer = styled.div`
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+`
 
 const StyledLiquidation = styled(Liquidation)`
   margin-right: 5px;
@@ -71,7 +80,7 @@ const MainCardContainer = styled.div`
   background: #FFFFFF;
   box-shadow: 0 4px 10px rgba(91, 78, 230, 0.25);
   border-radius: 20px;
-  
+
   p {
     font-weight: 500;
     font-size: 18px;
@@ -80,14 +89,98 @@ const MainCardContainer = styled.div`
 `;
 
 const CardContainer = styled.div`
-    display: flex;
+  display: flex;
   margin-top: 33px;
 `
 
+const ValueCircle = styled.div`
+  margin-top: 25px;
+  font-size: 10px;
+  //width: 50%;
+  width: 180px;
+  height: 50px;
+  padding: 9px 35px 9px 35px;
+  display: flex;
+  flex-direction: column;
+  color: #C4C4C4;
+  background: #4B4B62;
+  border-radius: 25px;
+  align-items: flex-end;
+
+  p {
+    margin-top: 2px;
+    display: block;
+    font-weight: 500;
+    font-size: 14px;
+    color: #FFFFFF;
+
+    type {
+      font-size: 12px;
+    }
+  }
+
+  & + & {
+    margin-left: 20px;
+  }
+`
+
+const CircleContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
+
+const SecondMainCardContainer = styled.div`
+  width: 100%;
+  height: 50%;
+  font-size: 18px;
+  font-weight: 700;
+  color: #222222;
+  padding: 60px 40px 36px 40px;
+  margin-top: 21px;
+  background: #FFFFFF;
+  box-shadow: 0 4px 10px rgba(91, 78, 230, 0.25);
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  p {
+    font-weight: 500;
+    font-size: 18px;
+    color: #777777;
+  }
+`;
+
+const SecondCardTextContainer = styled.div`
+  margin-top: 15px;
+  font-weight: 500;
+  font-size: 14px;
+  color: #777777;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  type {
+    margin-right: 5px; 
+    font-weight: 700;
+    font-size: 18px;
+    color: #222222;
+  }
+`
+
+const CardTextContainer = styled.div`
+  margin-top: 43px;
+`
 
 function BorrowTemplate() {
     const [collateralValue, setCollateralValue] = useState('5,691.595');
-    const [borrowedValue, setBorrowedValue] = useState(2127)
+    const [borrowedValue, setBorrowedValue] = useState(2127);
+    const [netaprValue, setNetaprValue] = useState(19);
+    const [borrowAprValue, setBorrowAprValue] = useState(11.8);
+    const [distributionAprValue, setDistributionAprValue] = useState(9.67);
+    const [borowedUSDT, setBorowedUSDT] = useState(0);
+    const [price, setPrice] = useState('55,382')
+    const [provided, setProvided] = useState(0)
 
     const handleConnectWallet = () => {
         console.log('ConnectWallet Button Click');
@@ -123,11 +216,62 @@ function BorrowTemplate() {
                 </FirstLineContainer>
 
                 <CardContainer>
-                    <BorrowBoard>{collateralValue}</BorrowBoard>
-                    <BorrowBoard>{borrowedValue}</BorrowBoard>
+                    <BorrowBoard sign="$" title="Collateral Value"
+                                 valuePosition="front">{collateralValue} </BorrowBoard>
+                    <BorrowBoard sign="$" title="Borrowed Value" valuePosition="front">{borrowedValue}
+                        <CircleContainer>
+                            <ValueCircle>
+                                Borowed
+                                <p>{borowedUSDT}
+                                    <type>USDT</type>
+                                </p>
+                            </ValueCircle>
+                        </CircleContainer>
+                    </BorrowBoard>
+                    <SecondBorrowBoard sign="%" title="NET APR">{netaprValue}
+                        <CircleContainer>
+                            <ValueCircle>
+                                Borrow APR
+                                <p>{borrowAprValue}</p>
+                            </ValueCircle>
+                            <ValueCircle>
+                                Distribution APR
+                                <p>{distributionAprValue}</p>
+                            </ValueCircle>
+                        </CircleContainer>
+                    </SecondBorrowBoard>
                 </CardContainer>
             </MainCardContainer>
 
+            <SecondMainCardContainer>
+                <BNear/>
+                bNEAR
+                <CardTextContainer>
+                    <SecondCardTextContainer>
+                        <p>Price</p>
+                        <p>
+                            <type>{price}</type>
+                            USDT
+                        </p>
+                    </SecondCardTextContainer>
+                    <SecondCardTextContainer>
+                        <p>Provided</p>
+                        <p>
+                            <type>{provided}</type>
+                            USDT
+                        </p>
+                    </SecondCardTextContainer>
+
+                    <CardButtonContainer>
+                        <CommonButton back={false}>Provide</CommonButton>
+                        <CommonButton back={false}>Withdraw</CommonButton>
+                    </CardButtonContainer>
+                </CardTextContainer>
+
+            </SecondMainCardContainer>
+            <TransitionAlerts>bAssets that have been transferred to Terra through Wormhole (e.g. webETH)
+                must go
+                through the convert operation to be used as collateral on Anchor. </TransitionAlerts>
         </Container>
     );
 }
